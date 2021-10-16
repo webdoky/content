25c1ab921e1568cc450d1b6fdc0fb57da619afa4
Vitalii Perehonchuk <brute18@gmail.com>
2021-10-16 17:17:45 +0300

    Refactoring & linting of code (#67)
    * Check of markdown, readme, rich customization
    
    * LanguageTool check: linting & refactor
    
    * fix inconsistencies
    
    * refactored check
    
    * Improved check
    
    Co-authored-by: Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>

---
+++ .eslintrc.cjs
@@ -0,0 +1,141 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:editorconfig/noconflict',
+    'plugin:promise/recommended',
+    'plugin:array-func/all',
+    'plugin:node/recommended',
+    'plugin:security-node/recommended',
+    'plugin:unicorn/all',
+    'plugin:eslint-comments/recommended',
+    'plugin:prettier/recommended',
+    'plugin:jest/all',
+    'prettier',
+  ],
+  overrides: [
+    {
+      files: ['./*', 'setup-tests.js', 'setupTests.js'],
+      rules: {
+        'import/no-extraneous-dependencies': [
+          'error',
+          {
+            devDependencies: true,
+          },
+        ],
+        'node/no-extraneous-import': 'off',
+        'node/no-unpublished-import': 'off',
+        'node/no-unpublished-require': 'off',
+        'no-console': 'off',
+      },
+    },
+    {
+      env: {
+        'jest/globals': true,
+      },
+      files: ['**/*.test.js'],
+      rules: {
+        'import/no-extraneous-dependencies': [
+          'error',
+          {
+            devDependencies: true,
+          },
+        ],
+        'no-magic-numbers': 'off',
+        'node/no-extraneous-import': 'off',
+      },
+    },
+    {
+      env: {
+        'jest/globals': true,
+      },
+      files: ['**/__mocks__/**'],
+      rules: {
+        'import/prefer-default-export': 'off',
+        'no-magic-numbers': 'off',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  plugins: [
+    'prettier',
+    'promise',
+    'unicorn',
+    'array-func',
+    'node',
+    'eslint-comments',
+    'jest',
+    'simple-import-sort',
+    'editorconfig',
+    'security-node',
+  ],
+  root: true,
+  rules: {
+    'no-param-reassign': ['error', { props: false }],
+    // 'consistent-return': 'off',
+    // 'arrow-body-style': 0,
+    // 'comma-dangle': 0,
+    // 'import/prefer-await-to-then': 'off',
+    // 'no-underscore-dangle': 'off',
+    'unicorn/no-null': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'no-void': ['error', { allowAsStatement: true }],
+    'no-magic-numbers': [
+      'error',
+      {
+        ignore: [0, 1, -1],
+        ignoreDefaultValues: true,
+      },
+    ],
+    'no-console': 'off',
+    'jest/prefer-expect-assertions': 'off',
+    'jest/no-conditional-expect': 'off',
+    'jest/expect-expect': 'off',
+    'jest/prefer-strict-equal': 'off',
+    'unicorn/prefer-spread': 'off',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Node.js builtins.
+          // eslint-disable-next-line global-require
+          [`^(${require('module').builtinModules.join('|')})(/|$)`],
+          // Packages.
+          ['^@?(\\w|.)[^./]'],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+    'import/order': 'off',
+    'no-loss-of-precision': 'warn',
+    'promise/no-nesting': 'error',
+    'unicorn/prefer-node-protocol': 'off',
+    'security-node/detect-crlf': 'warn',
+    'unicorn/no-unsafe-regex': 'warn',
+    'unicorn/prefer-at': 'off',
+    'unicorn/no-process-exit': 'off',
+    'no-process-exit': 'off',
+    'unicorn/no-array-for-each': 'off',
+    'unicorn/prefer-top-level-await': 'off',
+    'unicorn/no-array-reduce': 'off',
+  },
+};
--- scripts/utils/language-tool.js
+++ scripts/utils/language-tool.js
@@ -1,48 +1,50 @@
-import axios from "axios";
-import disabledRules from "./disabled-rules.js";
-import execute from "./execute.js";
-import sleep from "./sleep.js";
+import axios from 'axios';
 
-const START_COMMAND = "docker run -d -p 8010:8010 lt-custom";
+import debug from './debug';
+import disabledRules from './disabled-rules';
+import execute from './execute';
+import withRetryUntilTimeout from './retry-until-timeout';
+
+const START_COMMAND = 'docker run -d -p 8010:8010 lt-custom';
 const STOP_COMMAND =
-  process.platform === "win32"
+  process.platform === 'win32'
     ? 'FOR /f "tokens=*" %i IN (\'docker ps -a -q --filter ancestor=lt-custom --format="{{.ID}}"\') DO docker stop %i'
     : 'docker stop $(docker ps -a -q --filter ancestor=lt-custom --format="{{.ID}}")';
 const TIMEOUT = 8000;
 
+function requestLanguageTool(data) {
+  const parameters = new URLSearchParams();
+  Object.entries(data).forEach(([key, value]) => parameters.append(key, value));
+  // Disables check for unrecommended words
+  parameters.append('disabledRules', disabledRules.join(','));
+  return axios.post('http://localhost:8010/v2/check', parameters, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+}
+
 async function checkLanguageTool() {
   try {
     const result = await requestLanguageTool({
-      language: "en-US",
-      text: "Hello",
+      language: 'en-US',
+      text: 'Hello',
     });
-    console.debug(result);
-    return result.statusText === "OK";
-  } catch (error) {
+    debug(result);
+    return result.statusText === 'OK';
+  } catch {
     return false;
   }
 }
 
 export async function checkText(text) {
-  console.debug("checkText(...)");
+  debug('checkText(...)');
   return (
     await requestLanguageTool({
-      language: "uk-UA",
+      language: 'uk-UA',
       text,
     })
   ).data.matches;
-}
-
-function requestLanguageTool(data) {
-  const params = new URLSearchParams();
-  Object.entries(data).forEach(([key, value]) => params.append(key, value));
-  // Disables check for unrecommended words
-  params.append("disabledRules", disabledRules.join(','));
-  return axios.post("http://localhost:8010/v2/check", params, {
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-  });
 }
 
 export function startLanguageTool() {
@@ -53,17 +55,7 @@ export function stopLanguageTool() {
   return execute(STOP_COMMAND);
 }
 
-export function waitForLanguageTool() {
-  return Promise.race([
-    new Promise(async (resolve) => {
-      while (true) {
-        if (await checkLanguageTool()) {
-          resolve();
-          return;
-        }
-        await sleep(200);
-      }
-    }),
-    new Promise((resolve, reject) => setTimeout(reject, TIMEOUT)),
-  ]);
-}
+export const waitForLanguageTool = withRetryUntilTimeout(
+  checkLanguageTool,
+  TIMEOUT,
+);
